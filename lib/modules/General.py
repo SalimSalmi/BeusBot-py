396e206500cdc8229567b80bcf10cd4ebfba96df
Salim <salmi0707@gmail.com>
2017-05-21 14:55:38 +0200

    uptime

--- lib/modules/General.py
+++ lib/modules/General.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 from time import sleep
+import datetime
 
 class General:
     """General commands."""
@@ -16,3 +17,31 @@ class General:
             await self.bot.say(count-i)
             sleep(1)
         await self.bot.say('Go!')
+
+    @commands.command()
+    async def uptime(self):
+        """Show Beus' uptime."""
+        since = self.bot.uptime.strftime("%Y-%m-%d %H:%M:%S")
+        passed = self.get_bot_uptime()
+        message = discord.Embed(description="**{}**".format(passed),color=0x30b030)
+        await self.bot.say(embed=message)
+
+    def get_bot_uptime(self, *, brief=False):
+        # Courtesy of Danny
+        now = datetime.datetime.utcnow()
+        delta = now - self.bot.uptime
+        hours, remainder = divmod(int(delta.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+
+        if not brief:
+            if days:
+                fmt = '{d} days, {h} hours, {m} minutes, and {s} seconds'
+            else:
+                fmt = '{h} hours, {m} minutes, and {s} seconds'
+        else:
+            fmt = '{h}h {m}m {s}s'
+            if days:
+                fmt = '{d}d ' + fmt
+
+        return fmt.format(d=days, h=hours, m=minutes, s=seconds)
